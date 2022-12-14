e210560ea80036e6531a4648b5ada4f9b12ed036
Renat <jeingo20@gmail.com>
2022-12-14 23:36:31 +0300

    test vercel

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+    res.send('WORKS')
+})
+
 app.listen(PORT, () => {
     console.log(`Server is starting on port: ${PORT}`)
 })
