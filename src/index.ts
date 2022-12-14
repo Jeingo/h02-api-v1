b87c63df476890621173f3031c217fc41994da0b
Renat <jeingo20@gmail.com>
2022-12-14 23:43:51 +0300

    add http statuses constants

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('WORKS')
-})
-
 app.listen(PORT, () => {
     console.log(`Server is starting on port: ${PORT}`)
 })
