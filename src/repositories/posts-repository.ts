c53f8b56843d37c2306dd5952e84e0fdc53970f9
Renat <jeingo20@gmail.com>
2022-12-15 13:55:45 +0300

    add connection with field post.blogName and blog.name

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -10,25 +10,30 @@ export const postsRepository = {
         return foundPost
     },
     createPost(title: string, desc: string, content: string, blogId: string) {
-        const createdPost = {
-            id: uuid(),
-            title: title,
-            shortDescription: desc,
-            content: content,
-            blogId: blogId,
-            blogName: 'Name'
+        const foundBlog = db.blogs.find(b => b.id === blogId)
+        if(foundBlog) {
+            const createdPost = {
+                id: uuid(),
+                title: title,
+                shortDescription: desc,
+                content: content,
+                blogId: blogId,
+                blogName: foundBlog.name
+            }
+            db.posts.push(createdPost)
+            return createdPost
         }
-        db.posts.push(createdPost)
-        return createdPost
+        return null
     },
     updatePost(id: string, title: string, desc: string, content: string, blogId: string) {
+        const foundBlog = db.blogs.find(b => b.id === blogId)
         const foundPost = db.posts.find(p => p.id === id)
-        if(foundPost) {
+        if(foundPost && foundBlog) {
             foundPost.title = title
             foundPost.shortDescription = desc
             foundPost.content = content
             foundPost.blogId = blogId
-            foundPost.blogName = 'Name NEW'
+            foundPost.blogName = foundBlog.name
             return foundPost
         }
         return null
