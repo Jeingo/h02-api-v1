97a6207c3c1674b6f9c9a90c348593692b557b68
Renat <jeingo20@gmail.com>
2022-12-17 00:03:03 +0300

    write all blogs router tests

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {testRouter} from "./routers/test-router"
 
 export const app = express()
 
-const PORT = process.env.PORT || 9999
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
