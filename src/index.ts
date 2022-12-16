7f21b6a555e0d15bdfdfb794c829697fd1cc1645
Renat <jeingo20@gmail.com>
2022-12-16 21:54:50 +0300

    fix testing process

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import {blogsRouter} from "./routers/blogs-router"
 import {postsRouter} from "./routers/posts-router"
 import {testRouter} from "./routers/test-router"
@@ -10,11 +9,12 @@ const PORT = process.env.PORT || 9999
 
 app.use(express.json())
 
-
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 app.use('/testing/all-data', testRouter)
 
-app.listen(PORT, () => {
-    console.log(`Server is starting on port: ${PORT}`)
-})
+if(require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is starting on port: ${PORT}`)
+    })
+}
